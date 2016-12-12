94d2e6a3e539ba875dbac4bec62dd76327ce1e03
Vadim Shovkun <007_assasin_007@ukr.net>
2016-12-12 12:09:07 +0200

    lab4 commit

---
+++ usermanagement/src/main/java/com/shovkun/usermanagement/web/AddServlet.java
@@ -0,0 +1,23 @@
+package com.shovkun.usermanagement.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.shovkun.usermanagement.User;
+import com.shovkun.usermanagement.db.DaoFactory;
+import com.shovkun.usermanagement.db.DatabaseException;
+
+public class AddServlet extends EditServlet {
+
+	protected void processUser(User user) throws DatabaseException {
+        DaoFactory.getInstance().getUserDao().create(user);
+    }
+	
+	protected void showPage(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/add.jsp").forward(req, resp);
+    }
+}
